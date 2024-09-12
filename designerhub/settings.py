8d2605630d228fd7edcae0dbfa239e2bf6bdd19e
Farha Islam <farha.islam@students.codeinstitute.net>
2024-09-12 07:51:27 +0000

    Add: Settings for deployment

--- designerhub/settings.py
+++ designerhub/settings.py
@@ -41,6 +41,11 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y',
 }
 
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
@@ -58,7 +63,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost','8000-farhatamann-designerhub-wvl0ehiips6.ws.codeinstitute-ide.net']
 
