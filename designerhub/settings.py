4b58e0233697854a25ad0401ce3a4d6368fd0955
Farha Islam <farha.islam@students.codeinstitute.net>
2024-09-12 07:35:51 +0000

    Add: Settings for deployment

--- designerhub/settings.py
+++ designerhub/settings.py
@@ -59,7 +59,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-farhatamann-designerhub-wvl0ehiips6.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost','8000-farhatamann-designerhub-wvl0ehiips6.ws.codeinstitute-ide.net']
 
 
 # Application definition
