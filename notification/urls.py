d3f1fac7b9125a25a873842e66620218805eefb7
Farha Islam <farha.islam@students.codeinstitute.net>
2024-09-17 17:49:12 +0000

    Fix: Notification bug

--- notification/urls.py
+++ notification/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from notification import views
 
 urlpatterns = [
-    path('notification/', views.NotificationList.as_view()),
-    path('notification/<int:pk>/', views.NotificationUpdate.as_view())
+    path('notifications/', views.NotificationList.as_view()),
+    path('notifications/<int:pk>/', views.NotificationUpdate.as_view())
 ]
